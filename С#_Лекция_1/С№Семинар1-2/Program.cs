a629545cb11edbb8853eb2c62be2aa5ebd956d5d
Evgeniy Kuznetsov <kuznetsovevgeniyolegovich@mail.ru>
2022-05-29 13:03:38 +0300

    all works finally

--- С#_Лекция_1/С№Семинар1-2/Program.cs
+++ С#_Лекция_1/С№Семинар1-2/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hi!Enter a day number: ");
+Console.WriteLine("Hi!Enter a day number");
+int a = Convert.ToInt32(Console.ReadLine());
+ if(a==1){Console.WriteLine("Its monday");}
+else if (a==2){Console.WriteLine("U2sday");}
+else if (a==3){Console.WriteLine("WeeDnesday");}
+else if (a==4){Console.WriteLine("Thursday");}
+else if (a==5){Console.WriteLine("Thank God its friday");}
+else if (a==6){Console.WriteLine("Saturday");}
+else if (a==7){Console.WriteLine("Sunday");}
+
+
+
+
+
 
