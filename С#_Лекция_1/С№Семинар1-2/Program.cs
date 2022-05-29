2f8e58e0c3e3f07dc615a69497eef0874194195a
Evgeniy Kuznetsov <kuznetsovevgeniyolegovich@mail.ru>
2022-05-29 13:05:32 +0300

    else and wrong number works

--- С#_Лекция_1/С№Семинар1-2/Program.cs
+++ С#_Лекция_1/С№Семинар1-2/Program.cs
@@ -8,6 +8,7 @@ else if (a==4){Console.WriteLine("Thursday");}
 else if (a==5){Console.WriteLine("Thank God its friday");}
 else if (a==6){Console.WriteLine("Saturday");}
 else if (a==7){Console.WriteLine("Sunday");}
+else {Console.WriteLine("Wrong number");}
 
 
 
