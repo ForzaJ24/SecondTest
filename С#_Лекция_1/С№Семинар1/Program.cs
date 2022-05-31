a86dcf9bcc64d1326b05bba6bcf3b06ab550dc71
Evgeniy Kuznetsov <105036824+ForzaJ24@users.noreply.github.com>
2022-05-31 10:32:03 +0300

    Delete С#_Лекция_1/С№Семинар1 directory

--- С#_Лекция_1/С№Семинар1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// Checking squere of number "a"
-
-Console.WriteLine("Enter a number a: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter a number b: ");
-int b = Convert.ToInt32(Console.ReadLine());
-if (b==a*a){Console.WriteLine("Yes,Boss!");}
-   else{Console.WriteLine("No.");}
